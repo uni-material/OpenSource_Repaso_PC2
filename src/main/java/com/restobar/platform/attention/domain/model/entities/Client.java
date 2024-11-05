a45692d40c1eb78397ad58f5cff697a0abc9cb13
RominaMaita <u202213765@upc.edu.pe>
2024-11-05 12:16:05 -0500

    feat: add client entity.

---
+++ src/main/java/com/restobar/platform/attention/domain/model/entities/Client.java
@@ -0,0 +1,23 @@
+package com.restobar.platform.attention.domain.model.entities;
+
+import com.restobar.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client extends AuditableAbstractAggregateRoot<Client> {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String fullName;
+
+
+
+
+}
